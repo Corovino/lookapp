71131ee774cba6113983d6c702f45afec69d0348
Hebert Romero <hebert@lookapp.com.co>
2019-04-04 09:33:37 -0500

    ajuste conflictos merege develop origin

--- src/pages/my-tasks/my-tasks.ts
+++ src/pages/my-tasks/my-tasks.ts
@@ -95,21 +95,10 @@ export class MyTasksPage {
     })
   }
 
-<<<<<<< HEAD
-
-
-
-
-  razon(data: any) {
-    this.repo.presentAlert("Razón del rechazo", data.observations ,  Message_rpt.RTP_CLS_ACCEPT)
-  }
-
-=======
   razon(data: any) {
       this.repo.presentAlert(data.comments.observations, [Message_rpt.RTP_ACCEPT], Message_rpt.RTP_CLS_ACCEPT)
   }
 
->>>>>>> f71371f3fd10a634c4f797c4106a11969506d4c5
   doRefresh(element){
     this.get_task();
     setTimeout(() => {
